d5798e3eacb1ffde6dff02c8832d9a5733e5e194
Christopher <onlygamernerd@gmail.com>
2021-05-01 17:21:49 +0200

    feat(api/login): update login api'

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,3 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { handleLogin } from '../../lib/user';
 
 // uhhh..whats going on here?
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { username, password } = req.body;
+  try {
+    const user = await handleLogin(username, password);
+
+    res.status(201).json({ user: user.uid });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
