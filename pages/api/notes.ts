8b59a52b230e988cba74154c2b86e3896cf2bb44
Christopher <onlygamernerd@gmail.com>
2021-05-03 16:44:19 +0200

    fix typing

--- pages/api/notes.ts
+++ pages/api/notes.ts
@@ -5,5 +5,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { uid, title, content } = req.body;
   await handleCreateNote(uid, title, content);
 
-  res.status(200).json({ message: 'Creted a new note!' });
+  res.status(200).json({ message: 'Created a new note!' });
 };
